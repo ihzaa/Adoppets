743e8e32b5491a899ac08f1c0bdd1cc4e3ffbbd5
Naufaldi1705 <naufaldi.firmana@gmail.com>
2021-02-26 14:36:53 +0700

    revisi tampilan submit posting

--- resources/views/user/submit/submit.blade.php
+++ resources/views/user/submit/submit.blade.php
@@ -16,7 +16,7 @@
 @endsection
 
 @section('brand-logo')
-{{asset('user/assets/img/include_image/logo_adoptpets-inverted.png')}}
+{{asset('user/assets/img/include_image/logo_adoptpets.png')}}
 @endsection
 
 @section('hero-form')
