28a29aa37d872104c532438936ae76b5c5d10cb2
Naufaldi1705 <naufaldi.firmana@gmail.com>
2021-03-06 11:51:14 +0700

    membuat detail info pengadopsi

---
+++ resources/views/user/account/infopengadopsi.blade.php
@@ -0,0 +1,111 @@
+@extends('user/master')
+
+
+@section('nama-page')
+sub-page
+@endsection
+
+
+
+@section('page-title')
+<div class="page-title">
+    <div class="container">
+        <h1><strong>Daftar Calon Pengadopsi</strong></h1>
+    </div>
+    <!--end container-->
+</div>
+@endsection
+
+@section('brand-logo')
+{{asset('user/assets/img/include_image/logo_adoptpets.png')}}
+@endsection
+
+@section('hero-form')
+
+@endsection
+
+@section('background')
+<div class="background">
+    <div class="background-image">
+        <img src="{{asset('user/assets/img/include_image/bg_akunsaya.jpg')}}" alt="">
+    </div>
+    <!--end background-image-->
+</div>
+@endsection
+
+@section('content')
+<section class="block">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h3><i class="fa fa-user"></i> Profile Pengguna</h3>
+
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td>Nama</td>
+                                    <td width="10">:</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>Alamat Sekarang</td>
+                                    <td>:</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>Alamat Domisili</td>
+                                    <td>:</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>Email</td>
+                                    <td>:</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>No Telepon</td>
+                                    <td>:</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>No Whatsapp</td>
+                                    <td>:</td>
+                                    <td></td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 mt-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h3><i class="fa fa-history"></i> Riwayat Pengguna</h3>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <!-- <th>Gambar</th> -->
+                                    <th>Tanggal</th>
+                                    <th>Hewan</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
