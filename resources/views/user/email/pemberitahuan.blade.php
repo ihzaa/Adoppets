aa2054a5459a860ef806c66288287cf49cc3a1b9
Ulfah Nur Oktaviana <ulfanuroktaviana575@webmail.umm.ac.id>
2021-03-11 22:36:41 +0700

    email versi 2

--- resources/views/user/email/pemberitahuan.blade.php
+++ resources/views/user/email/pemberitahuan.blade.php
@@ -57,7 +57,7 @@
                                                         valign="top">
                                                         Terdapat <strong
                                                             style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">jumlah
-                                                            free rep</strong> remaining.
+                                                            peminat</strong> hewan anda.
                                                     </td>
                                                 </tr>
                                                 <tr
@@ -65,8 +65,15 @@
                                                     <td class="content-block"
                                                         style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;"
                                                         valign="top">
-                                                        Add your credit card now to upgrade your account to a premium
-                                                        plan to ensure you don't miss out on any reports.
+                                                        1. Nama Pengadopsi Pertama
+                                                    </td>
+                                                </tr>
+                                                <tr
+                                                    style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                                    <td class="content-block"
+                                                        style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;"
+                                                        valign="top">
+                                                        2. Nama Pengadopsi Kedua
                                                     </td>
                                                 </tr>
                                                 <tr
@@ -75,8 +82,8 @@
                                                         style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;"
                                                         valign="top">
                                                         <a href="#" class="btn-primary"
-                                                            style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; color: #FFF; text-decoration: none; line-height: 2em; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px; text-transform: capitalize; background-color: #f1556c; margin: 0; border-color: #f1556c; border-style: solid; border-width: 8px 16px;">Upgrade
-                                                            my account</a>
+                                                            style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; color: #FFF; text-decoration: none; line-height: 2em; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px; text-transform: capitalize; background-color: #f1556c; margin: 0; border-color: #f1556c; border-style: solid; border-width: 8px 16px;">Informasi
+                                                            Lengkap</a>
                                                     </td>
                                                 </tr>
                                                 <tr
@@ -84,7 +91,8 @@
                                                     <td class="content-block"
                                                         style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;"
                                                         valign="top">
-                                                        Thanks for choosing <b>Minton</b> Admin.
+                                                        Klik tombol diatas untuk memilih orang yang akan mengadopsi
+                                                        hewan anda <br> <br> <b>Adopt Pets</b> Admin.
                                                     </td>
                                                 </tr>
                                             </tbody>
