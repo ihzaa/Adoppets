24758d3a552309a1bd2c81196cb02cb7e9405f7d
Naufaldi1705 <naufaldi.firmana@gmail.com>
2021-03-19 09:24:12 +0700

    perbaikan tampilan upload postingan hewan

--- resources/views/user/submit/submitclinic.blade.php
+++ resources/views/user/submit/submitclinic.blade.php
@@ -60,8 +60,7 @@
 @section('content')
 <section class="block">
     <div class="container">
-        <form class="form form-submit" action="{{route('post_clinic')}}" method="POST" id="submitclinic"
-            enctype="multipart/form-data">
+        <form class="form form-submit" action="{{route('post_clinic')}}" method="POST" id="submitclinic" enctype="multipart/form-data">
             @csrf
             <section>
                 <h2>Konten</h2>
@@ -69,9 +68,7 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="nama_klinik" class="col-form-label">Nama Klinik</label>
-                            <input name="nama_klinik" type="text"
-                                class="form-control @error('nama_klinik') is-invalid @enderror" id="nama_klinik"
-                                placeholder="contoh : Klinik Bakti Husada" required value="{{old('nama_klinik')}}">
+                            <input name="nama_klinik" type="text" class="form-control @error('nama_klinik') is-invalid @enderror" id="nama_klinik" placeholder="contoh : Klinik Bakti Husada" required value="{{old('nama_klinik')}}">
                         </div>
                         @error('nama_klinik')
                         <div class="alert alert-danger">{{ $message }}</div>
@@ -81,9 +78,7 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="no_telepon" class="col-form-label">Nomer Telepon</label>
-                            <input name="no_telepon" type="number"
-                                class="form-control @error('no_telepon') is-invalid @enderror" id="no_telepon"
-                                placeholder="contoh : 03321245161" required value="{{old('no_telepon')}}">
+                            <input name="no_telepon" type="number" class="form-control @error('no_telepon') is-invalid @enderror" id="no_telepon" placeholder="contoh : 03321245161" required value="{{old('no_telepon')}}">
                         </div>
                         @error('no_telepon')
                         <div class="alert alert-danger">{{ $message }}</div>
@@ -93,8 +88,7 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="email" class="col-form-label">Email</label>
-                            <input name="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                id="email" placeholder="contoh : klinikhusada@gmail.com" value="{{old('email')}}">
+                            <input name="email" type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="contoh : klinikhusada@gmail.com" value="{{old('email')}}">
                         </div>
                         @error('email')
                         <div class="alert alert-danger">{{ $message }}</div>
@@ -108,8 +102,7 @@
                     <div class="col-md-8">
                         <div class="form-group">
                             <label for="deskripsi" class="col-form-label">Deskripsi</label>
-                            <textarea id="summernote" name="deskripsi"
-                                class="form-control  background @error('deskripsi') is-invalid @enderror">{{old('deskripsi')}}</textarea>
+                            <textarea id="summernote" name="deskripsi" class="form-control  background @error('deskripsi') is-invalid @enderror">{{old('deskripsi')}}</textarea>
                         </div>
                         @error('deskripsi')
                         <div class="alert alert-danger">{{ $message }}</div>
@@ -120,10 +113,8 @@
                             <div class="form-group">
                                 <label for="picture" class="col-form-label">Foto Klinik</label>
                                 <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="imgInp" value="{{old('picture')}}"
-                                        required name="picture" {{request()->is('*/clinic*')?"required":""}}>
-                                    <label class="custom-file-label" id="labelnya_gambar"
-                                        for="imgInp">{{request()->is('*/clinic*')?"Image Clinic":"Picture.jpg"}}</label>
+                                    <input type="file" class="custom-file-input" id="imgInp" value="{{old('picture')}}" required name="picture" {{request()->is('*/clinic*')?"required":""}}>
+                                    <label class="custom-file-label" id="labelnya_gambar" for="imgInp">{{request()->is('*/clinic*')?"Image Clinic":"Picture.jpg"}}</label>
                                     <small class="form-text text-muted">- Ukuran max 256KB</small>
                                     <small class="form-text text-muted">- Harus berupa gambar (format:
                                         jpg, jpeg, svg, jfif,
@@ -139,9 +130,7 @@
                         <div class="row-md">
                             {{-- request foto --}}
 
-                            <img id="blah"
-                                src="{{request()->is('*submitclinic*')?asset('images/default/picture.svg'):asset($clinic->picture)}}"
-                                class="img-fluid" src="" alt="image advertisement" />
+                            <img id="blah" src="{{request()->is('*submitclinic*')?asset('images/default/picture.svg'):asset($clinic->picture)}}" class="img-fluid" src="" alt="image advertisement" />
                             {{-- akhir request foto --}}
                         </div>
                     </div>
@@ -160,10 +149,8 @@
                 <!--end row-->
                 <div class="form-group">
                     <label for="input-location" class="col-form-label">Detail Lokasi</label>
-                    <input name="city" type="text" class="form-control" id="city" placeholder="Location"
-                        readonly="readonly" value="Jakarta" name="city">
-                    <span class="geo-location input-group-addon" data-toggle="tooltip" data-placement="top"
-                        title="Find My Position"><i class="fa fa-map-marker"></i></span>
+                    <input name="city" type="text" class="form-control" id="city" placeholder="Location" readonly="readonly" value="Jakarta" name="city">
+                    <span class="geo-location input-group-addon" data-toggle="tooltip" data-placement="top" title="Find My Position"><i class="fa fa-map-marker"></i></span>
                 </div>
                 <!--end form-group-->
                 <label>Map</label>
@@ -193,51 +180,50 @@
 <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
 
 {{-- summernote --}}
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
 </script>
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script>
     $('#summernote').summernote({
-    placeholder: 'Tulis Deskripsi Iklan Disini',
-    tabsize: 4,
-    height: 190,
-    minHeight: null,
-    maxHeight: null,
-    focus: true,
-    toolbar: [
-        ['style', ['style']],
-        ['font', ['bold', 'underline', 'clear']],
-        ['fontname', ['fontname']],
-        ['color', ['color']],
-        ['para', ['ul', 'ol', 'paragraph']],
-        ['table', ['table']],
-        ['insert', ['link', 'picture']],
-    ]
-});
+        placeholder: 'Tulis Deskripsi Iklan Disini',
+        tabsize: 4,
+        height: 190,
+        minHeight: null,
+        maxHeight: null,
+        focus: true,
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['fontname', ['fontname']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture']],
+        ]
+    });
 
-$(document).ready(function() {
-    $('#summernote').summernote();
-});
+    $(document).ready(function() {
+        $('#summernote').summernote();
+    });
 </script>
 
 <script>
     function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
-        reader.onload = function(e) {
-            $('#blah').attr('src', e.target.result);
+            reader.onload = function(e) {
+                $('#blah').attr('src', e.target.result);
+            }
+            $("#labelnya_gambar").html(input.files[0].name);
+            reader.readAsDataURL(input.files[0]); // convert to base64 string
         }
-        $("#labelnya_gambar").html(input.files[0].name);
-        reader.readAsDataURL(input.files[0]); // convert to base64 string
     }
-}
 
-$("#imgInp").change(function() {
-    readURL(this);
-});
+    $("#imgInp").change(function() {
+        readURL(this);
+    });
 </script>
 
 @endsection
